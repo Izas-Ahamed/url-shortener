3c8331cc4f80d9a69fbdc8de2958bfa68cbfcc90
THUNDERX <thunderxcodes@gmail.com>
2022-10-06 07:30:45 +0530

    changed config ⚙

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-require("dotenv").config();
 
 const authRouter = require("./routes/authRouter");
 const urlRouter = require("./routes/urlRouter");
